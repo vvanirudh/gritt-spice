d486e73f759d04adc97e8dfd35955a3e9052b4f6
Rishav Agarwal <5424122+rra94@users.noreply.github.com>
2026-02-12 18:26:32 +0530

    Update version.go

--- version.go
+++ version.go
@@ -35,7 +35,8 @@ func (cmd *versionCmd) Run(app *kong.Kong) error {
 	}
 
 	fmt.Fprintln(app.Stdout)
-	fmt.Fprintln(app.Stdout, "Copyright (C) vvanirudh")
+	fmt.Fprintln(app.Stdout, "Copyright (C) Abhinav Gupta")
+	fmt.Fprintln(app.Stdout, "Copyright (C) Gritt Robotics")
 	fmt.Fprintln(app.Stdout, "  <https://github.com/vvanirudh/gritt-spice>")
 	fmt.Fprintln(app.Stdout, "This program comes with ABSOLUTELY NO WARRANTY")
 	fmt.Fprintln(app.Stdout, "This is free software, and you are welcome to redistribute it")
