5fef5a59f4baffc0558c08bbfade601d937e3fd3
Rishav Agarwal <5424122+rra94@users.noreply.github.com>
2026-05-06 09:55:04 -0700

    Revert "Add `gs run precommit-checks` command for local CI verification (#20)"
    This reverts commit 190b84ab675890cf6c053d3cdab5a80b6621f51b.

--- run.go
+++
@@ -1,6 +0,0 @@
-package main
-
-// runCmd groups commands that run things locally.
-type runCmd struct {
-	PrecommitChecks runPrecommitChecksCmd `cmd:"precommit-checks" help:"Run configured local checks before push"`
-}
